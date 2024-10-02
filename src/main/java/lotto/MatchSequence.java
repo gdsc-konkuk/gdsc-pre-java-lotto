a2abfb6cde644a627ba7663fd494497a543bdc35
chabin37 <7chabin@naver.com>
2024-10-02 21:13:43 +0900

    🔨Refactor : 테스트 케이스를 위해 전체적 수정-MatchSequence 클래스

--- src/main/java/lotto/MatchSequence.java
+++ src/main/java/lotto/MatchSequence.java
@@ -6,38 +6,35 @@ import java.util.List;
 
 public class MatchSequence {
 
-    private List<Integer> prizeNum;
+    private Lotto prizeNum;
     private int bonus;
     private List<Lotto> buy;
     private List<Integer> result = new ArrayList<>(Collections.nCopies(5, 0));//3,4,5,6,5+bonus
 
-    public MatchSequence(List<Integer> prizeNum, int bonus, List<Lotto> buy) {
+    public MatchSequence(Lotto prizeNum, int bonus, List<Lotto> buy) {
         this.prizeNum = prizeNum;
         this.bonus = bonus;
         this.buy = buy;
     }
 
     public void Matching() {
-        Collections.sort(prizeNum);
         for (Lotto lotto : buy) {
-            Collections.sort(lotto.getNumbers());
             MatchCalculator(lotto);
         }
     }
 
     private void MatchCalculator(Lotto lt) {
-        int prizeindex = 0, buyindex = 0, correctCount = 0;
-        while (prizeindex < prizeNum.size() && buyindex < lt.getNumbers().size()) {
-            if (prizeNum.get(prizeindex).equals(lt.getNumbers().get(buyindex))) {
-                correctCount++;
-                prizeindex++;
-                buyindex++;
-            } else if (prizeNum.get(prizeindex) < lt.getNumbers().get(buyindex)) prizeindex++;
-            else if(prizeNum.get(prizeindex) > lt.getNumbers().get(buyindex)) buyindex++;
+        int correctCount = 0;
+        for (int i = 0; i < 6; i++) {
+            for(int j = 0; j < prizeNum.getNumbers().size(); j++) {
+                if(prizeNum.getNumbers().get(i)==lt.getNumbers().get(j)){
+                    correctCount++;
+                }
+            }
         }
-        if (correctCount == 5) {
+        if(correctCount==5){
             MatchBonusCalculator(lt);
-        } else if (correctCount > 2) {
+        }else if (correctCount > 2) {
             result.set(correctCount - 3, result.get(correctCount - 3) + 1);
         }
     }
