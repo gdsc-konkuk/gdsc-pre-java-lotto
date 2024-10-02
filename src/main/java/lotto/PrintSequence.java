352a4dd4db78c6ee5b273bfab00f497303c42660
chabin37 <7chabin@naver.com>
2024-10-02 21:13:35 +0900

    🔨Refactor : 테스트 케이스를 위해 전체적 수정-PrintSequence 클래스

--- src/main/java/lotto/PrintSequence.java
+++ src/main/java/lotto/PrintSequence.java
@@ -1,11 +1,9 @@
 package lotto;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class PrintSequence {
-    private List<Integer> result = new ArrayList<>(Collections.nCopies(5, 0));//3,4,5,6,5+bonus
+    private List<Integer> result ;//3,4,5,6,5+bonus
     private List<Lotto> buy;
     private double profitRate;
 
@@ -21,7 +19,7 @@ public class PrintSequence {
         System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n", result.get(4));
         System.out.printf("6개 일치 (2,000,000,000원) - %d개\n", result.get(3));
 
-        System.out.printf("총 수익률은 %.0f%%입니다.", profitRate);
+        System.out.printf("총 수익률은 %.1f%%입니다.", profitRate);
     }
     public double Calculate() {
         long totalProfit = result.get(0) * 5000L +
@@ -30,7 +28,7 @@ public class PrintSequence {
                 result.get(4) * 30000000L +
                 result.get(3) * 2000000000L;
         long investment = buy.size() * 1000L;
-        double profitRate = (double) totalProfit / investment * 100;
+        profitRate = (double) totalProfit / investment * 100;
         return profitRate;
     }
 }
