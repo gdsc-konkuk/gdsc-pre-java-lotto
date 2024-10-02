2203c0b2233cdc33219b8c2fb3f6ec9d9d781595
chabin37 <7chabin@naver.com>
2024-10-02 20:18:17 +0900

    🔨Refactor : 주석 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
+        //입력받기
         InputSequence is =new InputSequence(new Scanner(System.in),new Validation());
         is.PurchaseSequence();
         is.PrizeSequence();
         is.BonusSequence();
-
+        //계산하기
         MatchSequence ms=new MatchSequence(is.getPrizeNum(),is.getBonus(),is.getBuy());
         ms.Matching();
-
+        //출력하기
         PrintSequence ps=new PrintSequence(ms.getResult(),ms.getBuy());
         ps.Calculate();
         ps.ResultPrint();
