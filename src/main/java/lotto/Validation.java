d9fc7d1929a4f90a4560363d596bbee88c1520ee
chabin37 <7chabin@naver.com>
2024-10-02 21:12:19 +0900

    🔨Refactor : 테스트 케이스를 위해 전체적 수정-Validation 클래스

--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Validation {
@@ -7,46 +8,40 @@ public class Validation {
         if (s.matches("\\d+")) {
             int purchase = Integer.parseInt(s);
             if (purchase < 0 || purchase % 1000 != 0) {
-                System.out.println("[ERROR] 1000으로 나누어 떨어지는 자연수를 입력해야 합니다.");
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("[ERROR] 1000으로 나누어 떨어지는 자연수를 입력해야 합니다.");
             }
             return purchase;
         }
-        System.out.println("[ERROR] 자연수만 입력해야 합니다.");
-        throw new IllegalArgumentException();
+        throw new IllegalArgumentException("[ERROR] 자연수만 입력해야 합니다.");
     }
-    public void prize(String s, List<Integer> prizeNum){
+    public List<Integer> prize(String s){
+        ArrayList<Integer> list = new ArrayList();
         String[] st=s.split(",");
         if (st.length != 6) {
-            System.out.println("[ERROR] 6개의 자연수를 기호 {,}를 활용하여 구분해 입력해야 합니다.");
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 6개의 자연수를 기호 {,}를 활용하여 구분해 입력해야 합니다.");
         }
         for (String t : st) {
             int num = Integer.parseInt(t.trim());
             if (num < 1 || num > 45) {
-                System.out.println("[ERROR] 1~45 사이의 자연수를 입력해야 합니다.");
-                throw new IllegalArgumentException();
-            } else if (prizeNum.contains(num)) {
-                System.out.println("[ERROR] 중복되지 않는 값을 정해야 합니다.");
-                prizeNum.clear();
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("[ERROR] 1~45 사이의 자연수를 입력해야 합니다.");
+            } else if (list.contains(num)) {
+                list.clear();
+                throw new IllegalArgumentException("[ERROR] 중복되지 않는 값을 정해야 합니다.");
             }
-            prizeNum.add(num);
+            list.add(num);
         }
+        return list;
     }
-    public int Bonus(String s, List<Integer> prizeNum) {
+    public int Bonus(String s, Lotto prizeNum) {
         if (s.matches("\\d+")) {
             int bonus = Integer.parseInt(s);
             if(bonus<1||bonus>45){
-                System.out.println("[ERROR] 1~45 사이의 자연수를 입력해야 합니다.");
-                throw new IllegalArgumentException();
-            }else if(prizeNum.contains(bonus)){
-                System.out.println("[ERROR] 중복되지 않는 값을 정해야 합니다.");
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("[ERROR] 1~45 사이의 자연수를 입력해야 합니다.");
+            }else if(prizeNum.getNumbers().contains(bonus)){
+                throw new IllegalArgumentException("[ERROR] 중복되지 않는 값을 정해야 합니다.");
             }
             return bonus;
         }
-        System.out.println("[ERROR] 자연수만 입력해야 합니다.");
-        throw new IllegalArgumentException();
+        throw new IllegalArgumentException("[ERROR] 자연수만 입력해야 합니다.");
     }
 }
